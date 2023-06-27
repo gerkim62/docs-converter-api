00d8ed853f15cc0a4a4e197120cf7fe6e938c65e
Gerison Kimathi <gerkim62@gmail.com>
2023-06-27 22:27:05 +0300

    updated chunk osize and overlap

--- index.mjs
+++ index.mjs
@@ -9,7 +9,7 @@ import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
 const splitter = new RecursiveCharacterTextSplitter({
   chunkSize: 1000,
-  chunkOverlap: 1,
+  chunkOverlap: 200,
 });
 
 import mime from "mime";
